c0d248d07805f0d60e4b99f18b78068100d852a5
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-03-19 02:47:07 -0300

    Primieros passos

---
+++ CapgeminiSchool/LAIII/Aprendizado Logicas/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner leitor = new Scanner(System.in);
+
+        int idade = leitor.nextInt();
+        System.out.println(idade);
+    }
+}
