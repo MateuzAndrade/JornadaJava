3fd63a0ce6aa1906ff07aac783f9b1b033564827
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-04-08 21:21:15 -0300

    aulas

---
+++ CapgeminiSchool/LAIII/Classes/src/Funcionario.java
@@ -0,0 +1,17 @@
+public class Funcionario {
+    private String nome;
+    private int CPF;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+    public int getCPF() {
+        return CPF;
+    }
+}
--- CapgeminiSchool/LAIII/Classes/src/main.java
+++ CapgeminiSchool/LAIII/Classes/src/main.java
@@ -1,19 +1,8 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Pessoa objetoPessoa = new Pessoa();
 
-        Scanner leitor = new Scanner(System.in);
-
-        System.out.println("Digite seu Nome");
-        objetoPessoa.nome = leitor.next();
-        System.out.println("Informe seu Peso");
-        objetoPessoa.peso = leitor.nextFloat();
-        System.out.println("Informe sua Altura");
-        objetoPessoa.altura = leitor.nextFloat();
-
-        System.out.println("Olá "+objetoPessoa.nome+" Seu IMC é:"+objetoPessoa.calcularIMC());
 
     }
 }
