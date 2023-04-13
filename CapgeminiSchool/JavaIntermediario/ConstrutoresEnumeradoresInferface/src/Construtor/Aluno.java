b3f937212ba7a13d1326cf22f0194e2b7d42202b
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-04-13 06:53:39 -0300

    aulas

--- CapgeminiSchool/JavaIntermediario/ConstrutoresEnumeradoresInferface/src/Construtor/Aluno.java
+++ CapgeminiSchool/JavaIntermediario/ConstrutoresEnumeradoresInferface/src/Construtor/Aluno.java
@@ -4,30 +4,33 @@ public class Aluno {
     private double nota1;
     private double nota2;
     private double nota3;
-    STATUS situacaoAluno;
-
+    MATRICULA situacaoMatricula;
 
     public  Aluno (double nota1,double nota2,double nota3){
         this.nota1 = nota1;
         this.nota2 = nota2;
         this.nota3 = nota3;
     }
+    public enum MATRICULA {
+        MATRICULADO,
+        PENDENTE,
+        RECUSADO
+    }
 
     public double calculoDeMedia() {
         double media = 0;
         media = (this.nota1+this.nota2+this.nota3)/3;
-        return  media;
+        if (media >= 7){
+            System.out.println("APROVADO");
+        } else {
+            System.out.println("REPROVADO");
+        }
+        System.out.print("O Valor da Média foi:");
+        return media;
     }
 
     public enum STATUS{
         APROVADO,
         REPROVADO
     }
-    public void Resultado(){
-        if (calculoDeMedia >= 7){
-            System.out.println();
-        }
-    }
-
-
 }
--- CapgeminiSchool/JavaIntermediario/ConstrutoresEnumeradoresInferface/src/Construtor/Construtor.java
+++ CapgeminiSchool/JavaIntermediario/ConstrutoresEnumeradoresInferface/src/Construtor/Construtor.java
@@ -1,11 +1,13 @@
 package Construtor;
-
 public class Construtor {
     public static void main(String[] args) {
 
-        Aluno Jose = new Aluno(8,3,7);
+        Aluno Jose;
+        Jose = new Aluno(8,6,7);
         double resultado = Jose.calculoDeMedia();
         System.out.println(resultado);
+        Jose.situacaoMatricula = Aluno.MATRICULA.MATRICULADO;
+        System.out.println("O Aluno está: " + Jose.situacaoMatricula);
 
     }
 }
