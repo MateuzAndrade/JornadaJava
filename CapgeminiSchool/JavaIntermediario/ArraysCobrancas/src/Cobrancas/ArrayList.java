6c6afe9b8154d3810c76522c2f8c2487a95508b1
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-04-17 20:30:30 -0300

    aa

---
+++ CapgeminiSchool/JavaIntermediario/ArraysCobrancas/src/Cobrancas/ArrayList.java
@@ -0,0 +1,19 @@
+package Cobrancas;
+import  java.util.ArrayList;
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList carros = new ArrayList<>();
+        ArrayList number = new ArrayList<>();
+
+
+        //adicionando itens
+        carros.add("Celta");
+        carros.add("Fox");
+        carros.add("Kombi");
+
+        number.add(1);
+
+        System.out.println(carros);
+
+    }
+}
--- CapgeminiSchool/JavaIntermediario/ArraysCobrancas/src/Main.java
+++ CapgeminiSchool/JavaIntermediario/ArraysCobrancas/src/Main.java
@@ -1,16 +1,6 @@
-import Cobrancas.ArrayList;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Arrays e Cobranças");
-        ArrayList carros = new ArrayList<>();
-
-
-        //adicionando itens
-        carros.add("Celta");
-        carros.add("Fox");
-        carros.add("Kombi");
 
     }
 }
