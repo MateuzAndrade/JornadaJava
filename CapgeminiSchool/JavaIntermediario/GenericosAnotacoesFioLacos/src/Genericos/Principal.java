4ebb7a59e84ae44cd554ffbe4f6c0a79f363dd30
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-04-19 09:13:28 -0300

    GENERIC

---
+++ CapgeminiSchool/JavaIntermediario/GenericosAnotacoesFioLacos/src/Genericos/Principal.java
@@ -0,0 +1,23 @@
+package Genericos;
+import Genericos.Classes.Aeronaves;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Aeronaves air = new Aeronaves();
+        System.out.println("Informe o Número de Aeronaves");
+        int nrAeronaves = sc.nextInt();
+
+        //adicionando voo
+        for (int i = 0 ; i < nrAeronaves; i++){
+            int nrVoo = sc.nextInt();
+            air.addVoo(nrVoo);
+        }
+        System.out.println("O primeiro a decolar vai ser o nr : " + air.primeiroVoo());
+        air.listaVoos();
+        sc.close();
+    }
+}
