626621ada00d9127836993de8e9691c21f56db5c
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2023-09-09 07:46:23 -0300

    Aulas

--- OrientacaoObjetos/src/Principal.java
+++ OrientacaoObjetos/src/Principal.java
@@ -3,13 +3,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Filme meuFilme = new Filme("Cronicas de Narnia",2005,true,10.0,10,150);
-		
-//		meuFilme.nome = "Crônicas de Nárnia";
-//		meuFilme.anoDeLancamento = 2005;
-//		meuFilme.duracaoEmMinutos = 150;
-		
 		meuFilme.exibeFichaTecnica();
-		
+			
 
 	}
 
