1983645a6c8001c51b9bf85dfeaf00663602af51
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2023-09-10 08:14:35 -0300

    Aulas

--- OrientacaoObjetos/src/Filme.java
+++ OrientacaoObjetos/src/Filme.java
@@ -1,27 +1,86 @@
 
 public class Filme {
-	//Inserir caracteristicas comuns a todos os filmes
-	//Objetos é a referencia para a construção de um item
-	
+	// Inserir caracteristicas comuns a todos os filmes
+	// Objetos é a referencia para a construção de um item
+
 	private String nome;
 	private int anoDeLancamento;
 	private boolean incluidoNoPlano;
-	private double avaliacao;
+	private double somaDasAvaliacao;
 	private int totalDeAvaliacoes;
 	private int duracaoEmMinutos;
 	
-	Filme(String nome, int anoDeLançamento, boolean incluidoNoPlano, double avaliacao, int totalDeAvaliacoes,int duracaoEmMinutos){
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnoDeLancamento() {
+		return anoDeLancamento;
+	}
+
+	public void setAnoDeLancamento(int anoDeLancamento) {
+		this.anoDeLancamento = anoDeLancamento;
+	}
+
+	public boolean isIncluidoNoPlano() {
+		return incluidoNoPlano;
+	}
+
+	public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+		this.incluidoNoPlano = incluidoNoPlano;
+	}
+
+	public double getSomaDasAvaliacao() {
+		return somaDasAvaliacao;
+	}
+
+	public void setSomaDasAvaliacao(double somaDasAvaliacao) {
+		this.somaDasAvaliacao = somaDasAvaliacao;
+	}
+
+	public int getTotalDeAvaliacoes() {
+		return totalDeAvaliacoes;
+	}
+
+	public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+		this.totalDeAvaliacoes = totalDeAvaliacoes;
+	}
+
+	public int getDuracaoEmMinutos() {
+		return duracaoEmMinutos;
+	}
+
+	public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+		this.duracaoEmMinutos = duracaoEmMinutos;
+	}
+
+	Filme(String nome, int anoDeLançamento, boolean incluidoNoPlano, double avaliacao, int totalDeAvaliacoes,
+			int duracaoEmMinutos) {
 		this.nome = nome;
 		this.anoDeLancamento = anoDeLançamento;
 		this.incluidoNoPlano = incluidoNoPlano;
-		this.avaliacao = avaliacao;
 		this.totalDeAvaliacoes = totalDeAvaliacoes;
 		this.duracaoEmMinutos = duracaoEmMinutos;
 	}
 
 	void exibeFichaTecnica() {
-		
+
 		System.out.println("Nome do Filme: " + nome);
 		System.out.println("Ano de Lançamento: " + anoDeLancamento);
 	}
+
+	void avalia(double nota) {
+		somaDasAvaliacao += nota;
+		totalDeAvaliacoes ++;
+	}
+	
+	double mediaAvaliacoes() {
+		return somaDasAvaliacao/totalDeAvaliacoes;
+	}
+	
 }
--- OrientacaoObjetos/src/Principal.java
+++ OrientacaoObjetos/src/Principal.java
@@ -2,8 +2,15 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		Filme meuFilme = new Filme("Cronicas de Narnia",2005,true,10.0,10,150);
+		Filme meuFilme = new Filme("Cronicas de Narnia",2005,true,0,0,0);
 		meuFilme.exibeFichaTecnica();
+		meuFilme.avalia(7);
+		meuFilme.avalia(7.5);
+		meuFilme.avalia(6.5);
+		
+		System.out.println(meuFilme.getSomaDasAvaliacao());
+		System.out.println(meuFilme.getTotalDeAvaliacoes());
+		System.out.println(meuFilme.mediaAvaliacoes());
 			
 
 	}
