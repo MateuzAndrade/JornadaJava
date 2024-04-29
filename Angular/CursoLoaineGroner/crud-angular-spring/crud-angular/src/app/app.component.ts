9051aea828386eb9ff2d391e0eefb2a56026ee3a
¨mateus.andrade¨ <mateus.nascimento.andrade@gmail.com>
2024-04-29 07:03:05 -0300

    Inicio Aprendizado Angular

---
+++ Angular/CursoLoaineGroner/crud-angular-spring/crud-angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { routes } from './app.routes';
+import { Component } from '@angular/core';
+import { RouterOutlet, Routes } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,MatToolbarModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'crud-angular';
+}
