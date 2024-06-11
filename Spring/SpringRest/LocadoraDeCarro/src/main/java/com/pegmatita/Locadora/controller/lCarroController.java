307b3bff793751fd591fba593e5da4727af4269d
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2024-06-11 07:07:39 -0300

    Implementando outros metodos JPA

--- Spring/SpringRest/LocadoraDeCarro/src/main/java/com/pegmatita/Locadora/controller/lCarroController.java
+++ Spring/SpringRest/LocadoraDeCarro/src/main/java/com/pegmatita/Locadora/controller/lCarroController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pegmatita.Locadora.model.locadoraDeCarroModel.Carro;
@@ -43,9 +45,15 @@ public class lCarroController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping("/por-nome")
+    public List<Carro> buscaCarroMarca(String marca ) {
+        return carroRepository.findByMarca(marca);
+    }
     
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Carro criar(@RequestBody Carro carro) {
         return carroRepository.save(carro);
     }
